e5927b20573bc3200c7e3ef1edb7bc046760cf24
Asish Tom <asish@reckonsys.com>
2019-01-10 23:43:02 +0530

    Understanding concepts, shadowing vs mutation.

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -3,4 +3,35 @@ fn main() {
     println!("The value of x is: {}", x);
     x = 6;
     println!("The value of x is: {}", x);
+    const MAX_POINTS: u32 = 100_000;
+    println!("The value of MAX_POINTS is: {}", MAX_POINTS);
+    shadowing_test();
+    shadowing_test_1();
+    shadowing_vs_mutation();
+
+}
+
+
+fn shadowing_test() {
+    let x = 5;
+
+    let x = x + 1;
+
+    let x = x * 2;
+
+    println!("The value of x is: {}", x);
+}
+
+
+fn shadowing_test_1() {
+    let spaces = "   ";
+    let spaces = spaces.len();
+
+    println!("The space between words {}", spaces)
+}
+
+fn shadowing_vs_mutation() {
+    let mut spaces = "   ";
+    spaces = spaces.len();
+    println!("The space between words {}", spaces)
 }
